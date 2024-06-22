7226ad781c3c195ba2311cbdff95e8c4bc1a0b03
wooohyerim <hyerim3414@gmail.com>
2024-06-23 00:40:22 +0900

    feat : 회원가입 firebase 연동

--- src/pages/SignupForm.tsx
+++ src/pages/SignupForm.tsx
@@ -1,71 +1,219 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { auth } from "@/firebase/firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
 import Button from "@/components/common/Button";
+import { FirebaseError } from "firebase/app";
+
+type FormValues = {
+  image: string;
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  bio: string;
+};
 
 const SignupForm = () => {
   const navigate = useNavigate();
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const profileImg = watch("image");
+
+  // image 이름 가져오기
+  const getFileName = (fileList: any) => {
+    // console.log(fileList);
+    return fileList && fileList.length > 0 ? fileList[0]?.name : "";
+  };
+
+  const onSubmit = async ({ email, password, nickname, image }: FormValues) => {
+    console.log(email, password, nickname);
+    console.log(image);
+    try {
+      const credential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = credential.user;
+      console.log(user);
+
+      await updateProfile(user, {
+        displayName: nickname,
+      });
+    } catch (error: unknown) {
+      const err = error as FirebaseError;
+      switch (err.code) {
+        case "auth/invalid-email":
+          alert("이메일을 바르게 입력해주세요.");
+          break;
+        case "auth/weak-password":
+          alert("비밀번호가 너무 쉬워요.");
+          break;
+        case "auth/email-already-in-use":
+          alert("등록된 이메일 입니다.");
+          break;
+        default:
+          console.log("회원가입 실패");
+          break;
+      }
+    }
+  };
+
   const goToLogin = () => {
     navigate("/login");
   };
+
   return (
-    <div className="flex flex-col w-[450px] h-full my-10 mx-auto p-3">
-      {/* <span
+    <div className="flex flex-col w-[450px] h-full my-6 mx-auto p-3">
+      <span
         onClick={() => navigate(-1)}
-        className="w-[30px] text-[24px] font-bold text-[#543310] cursor-pointer"
+        className="w-[30px] text-[24px] font-bold text-[#543310] cursor-pointer lg:hidden"
       >
         &lt;{" "}
-      </span> */}
+      </span>
       <h1 className=" mb-6 text-center text-[36px] text-[#543310] font-IBMSemibold ">
         Sign Up
       </h1>
-      <form className="flex flex-col gap-4 w-[400px] h-full my-0 mx-auto p-4">
-        <div className="flex flex-col items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-5 w-[400px] h-full my-0 mx-auto p-4"
+      >
+        <div className="flex flex-col gap-1 h-[60px]">
           <label
-            htmlFor="inputFile"
-            className=" w-24 h-24 rounded-full pt-8 bg-[#8B8989] cursor-pointer text-center text-white"
+            htmlFor="image"
+            className="w-[130px] h-8 text-center p-1 bg-[#D1BB9E] rounded-[4px] text-white cursor-pointer hover:bg-[#74512D] "
           >
-            업로드
+            profile upload!
           </label>{" "}
-          <input type="file" id="inputFile" className="hidden" />
+          <div className="text-[#D1BB9E] pl-1 ">
+            {profileImg && profileImg.length > 0 && getFileName(profileImg)}
+          </div>
+          <input
+            type={"file"}
+            accept=".gif, .jpg, .png, .pdf, .jpeg"
+            {...register("image", { required: "이미지를 등록해주세요." })}
+            id={"image"}
+            className="hidden"
+          />
+          {errors.image && (
+            <span className="pl-1 text-[#ff0000] text-[12px]">
+              {errors.image?.message}
+            </span>
+          )}
         </div>
 
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 h-[80px]">
           <label className="text-[#636363] text-[12px]">Name</label>
           <input
-            className="w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
-            type="text"
+            {...register("name", {
+              minLength: 2,
+              required: "이름을 입력해주세요.",
+            })}
+            className={
+              "w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
+            }
+            type={"text"}
           />
+          {errors.name && (
+            <span className="pl-1 text-[#ff0000] text-[12px]">
+              {errors.name.message}
+            </span>
+          )}
         </div>
-        <div className="flex flex-col gap-1">
+
+        <div className="flex flex-col gap-1 h-[80px]">
           <label className="text-[#636363] text-[12px]">Nickname</label>
           <input
-            className="w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
-            type="text"
+            {...register("nickname", {
+              minLength: 2,
+              required: "닉네임을 입력해주세요.",
+            })}
+            className={
+              "w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
+            }
+            type={"text"}
           />
+          {errors.nickname && (
+            <span className="pl-1 text-[#ff0000] text-[12px]">
+              {errors.nickname.message}
+            </span>
+          )}
         </div>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 h-[80px]">
           <label className="text-[#636363] text-[12px]">Email</label>
           <input
-            className="w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
-            type="text"
+            {...register("email", {
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9-.]+$/,
+                message: "유효한 이메일 주소를 입력해주세요.",
+              },
+              required: "이메일을 입력해주세요.",
+            })}
+            className={
+              "w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
+            }
+            type={"text"}
           />
+          {errors.email && (
+            <span className="pl-1 text-[#ff0000] text-[12px]">
+              {errors.email.message}
+            </span>
+          )}
         </div>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 h-[80px]">
           <label className="text-[#636363] text-[12px]">Password</label>
           <input
-            className="w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
-            type="password"
+            {...register("password", {
+              pattern: {
+                value:
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                message: "영문 + 숫자 + 특수문자 8자 이상 입력해주세요.",
+              },
+              required: "비밀번호를 입력해주세요.",
+            })}
+            className={
+              "w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
+            }
+            type={"password"}
           />
+          {errors.password && (
+            <span className="pl-1 text-[#ff0000] text-[12px]">
+              {errors.password.message}
+            </span>
+          )}
         </div>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 h-[80px]">
           <label className="text-[#636363] text-[12px]">Comment</label>
-          <textarea className="w-full h-[100px] p-4 border-none bg-white outline-none rounded-xl resize-vertical" />
+          <input
+            {...register("bio", {
+              minLength: 3,
+              required: "인사말을 입력해주세요.",
+            })}
+            className={
+              "w-full h-[50px] p-4 border-none bg-white outline-none rounded-xl"
+            }
+            type={"text"}
+          />
+          {errors.bio && (
+            <span className="pl-1 text-[#ff0000] text-[12px]">
+              {errors.bio.message}
+            </span>
+          )}
         </div>
         <div className="flex flex-col gap-4 mt-8">
-          <Button className={""} type={"button"} title={"가입하기"} />
+          <Button className={""} type={"submit"} title={"가입하기"} />
           <Button
-            className={""}
+            className={
+              "hidden lg:block w-full h-[55px] p-2 border-none bg-[#543310] outline-none text-white text-lg font-bold rounded-[4px]"
+            }
             type={"button"}
             title={"돌아가기"}
             onClick={() => navigate(-1)}
