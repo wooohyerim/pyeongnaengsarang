fa1c6ba59ac96d6e6625a75769fc81430ec4204d
wooohyerim <hyerim3414@gmail.com>
2024-06-25 12:02:25 +0900

    ADD : Main, MyPage, UserList, FeedDetail, Post 페이지 추가 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginForm from "./pages/LoginForm";
 import SignupForm from "./pages/SignupForm";
+import MainFeed from "./pages/MainFeed";
+import UserList from "./pages/UserList";
+import MyPage from "./pages/MyPage";
+import FeedDetail from "./pages/FeedDetail";
+import Post from "./pages/Post";
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
+        <Route path="main" element={<MainFeed />} />
+        <Route path="user" element={<UserList />} />
+        <Route path="mypage" element={<MyPage />} />
+        <Route path="detail" element={<FeedDetail />} />
+        <Route path="post" element={<Post />} />
       </Routes>
     </Router>
   );
