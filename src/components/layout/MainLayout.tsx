59091c134542a053bbcba479c3a80a9c5b5d5232
wooohyerim <hyerim3414@gmail.com>
2024-06-25 23:24:48 +0900

    feat : 유저조회리스트 useQuery로 데이터 받아서 배열로 저장

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
 import Header from "../Header";
 import NavBar from "../NavBar";
 import useUserState from "@/store/useUserState";
 import { auth } from "@/firebase/firebase";
+import { useQuery } from "@tanstack/react-query";
+
+interface AuthData {
+  isLogin: boolean;
+  profileUrl: string;
+}
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { setIsLogin } = useUserState();
   const navigate = useNavigate();
   const [profileUrl, setProfileUrl] = useState("");
   const [isOpened, setIsOpened] = useState(false);
+  const user = auth.currentUser;
+  // console.log(user);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+  // 현재 유저 확인
+  const currentData = async () => {
+    auth.onAuthStateChanged((user) => {
       if (user) {
-        // console.log(user);
         setIsLogin(true);
         setProfileUrl(user.photoURL || "");
       } else {
@@ -24,12 +31,44 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         setProfileUrl("");
       }
     });
-  }, []);
+  };
 
-  const onClickLogout = () => {
-    alert("로그인 페이지로 이동합니다.");
-    auth.signOut();
-    navigate("/login");
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["authState"],
+    queryFn: currentData,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+  console.log("상위 레이아웃 data  => ", data);
+
+  // useEffect(() => {
+  //   auth.onAuthStateChanged((user) => {
+  //     if (user) {
+  //       // console.log(user);
+  //       setIsLogin(true);
+  //       setProfileUrl(user.photoURL || "");
+  //     } else {
+  //       console.log("로그아웃");
+  //       setIsLogin(false);
+  //       setProfileUrl("");
+  //     }
+  //   });
+  // }, [user]);
+
+  const onClickLogout = async () => {
+    try {
+      await auth.signOut();
+      alert("로그인 페이지로 이동합니다.");
+      navigate("/login");
+    } catch (error) {
+      console.log("logout 실패", error);
+    }
   };
 
   const goToMyPage = () => {
@@ -46,7 +85,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       {isOpened === true && (
         <NavBar onClickLogout={onClickLogout} setIsOpened={setIsOpened} />
       )}
-      {children}
+      <div className="p-3">{children}</div>
     </div>
   );
 };
--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,8 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { db } from "@/firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
 import MainLayout from "@/components/layout/MainLayout";
-import React from "react";
 
 const UserList = () => {
-  return <MainLayout>UserList</MainLayout>;
+  // users 문서에 저장된 모든 유저 가져오기
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    const users = querySnapshot.docs.map((doc: any) => ({
+      ...doc.data(), // 배열로 저장
+    }));
+    return users;
+  };
+
+  const { data } = useQuery({
+    queryKey: ["users"],
+    queryFn: getData,
+  });
+
+  console.log("유저조회페이지 => ", data);
+
+  return (
+    <MainLayout>
+      <div></div>
+    </MainLayout>
+  );
 };
 
 export default UserList;
