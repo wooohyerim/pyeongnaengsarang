338cce23e3115b3a30c301010cc2c12d5cb8f713
wooohyerim <hyerim3414@gmail.com>
2024-07-02 15:19:19 +0900

    ADD : 게시물에 유저 프로필 이미지 추가

--- src/pages/MainFeed.tsx
+++ src/pages/MainFeed.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import MainLayout from "@/components/layout/MainLayout";
 import { getAllPostData } from "@/hooks/getPostData";
 import { cn } from "@/lib/utils";
+import { getAllUserData } from "@/hooks/getUserData";
 
 interface PostValue {
   photoURL?: File[] | undefined;
@@ -16,12 +17,20 @@ interface PostValue {
 
 const MainFeed = () => {
   const navigate = useNavigate();
+
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["allPosts"],
     queryFn: getAllPostData,
   });
 
-  console.log("postData => ", data);
+  const { data: userData } = useQuery({
+    queryKey: ["users"],
+    queryFn: getAllUserData,
+  });
+
+  // console.log(userData);
+
+  // console.log("postData => ", data);
 
   if (isLoading) {
     return (
@@ -48,56 +57,66 @@ const MainFeed = () => {
   }
   return (
     <MainLayout>
-      <div className="flex flex-wrap justify-between h-screen gap-2 px-3 py-3">
+      <div className="flex flex-wrap justify-between h-screen px-3 py-3">
         {data && data.length > 0 ? (
-          data.map((posts: PostValue) => (
-            <div
-              key={posts.postId}
-              className="flex flex-col gap-4 w-[230px] h-[300px] cursor-pointer bg-white rounded-xl border shadow-md"
-            >
-              {posts.photoURL ? (
-                <img
-                  src={`${posts.photoURL}`}
-                  alt="img"
-                  className="w-full h-[150px] object-cover rounded-t-xl"
-                />
-              ) : (
-                ""
-              )}
+          data.map((posts: PostValue) => {
+            const postUser = userData?.find((user) => user.uid === posts.uid);
+            return (
               <div
-                className={cn(
-                  "w-full h-[100px] p-1",
-                  !posts.photoURL && "w-full h-[400px] p-1 rounded-t-xl"
-                )}
+                key={posts.postId}
+                className="flex flex-col justify-between  w-full h-[200px] rounded-xl border shadow-md"
               >
-                <p
-                  className=" text-[#74512D]
-                 font-bold"
-                >
-                  {" "}
-                  {posts.title}
-                </p>
-                <span
+                <div className="flex w-full h-[150px]">
+                  <div
+                    className={cn(
+                      "w-[300px] h-full p-2",
+                      !posts.photoURL && "w-full h-full p-2 rounded-t-xl"
+                    )}
+                  >
+                    <p className=" text-[#74512D] font-bold"> {posts.title}</p>
+                    <span
+                      className={cn(
+                        "text-[14px] text-[#A79277] text-ellipsis line-clamp-5 text-balance"
+                      )}
+                    >
+                      {posts.content}
+                    </span>
+                  </div>
+                  {posts.photoURL ? (
+                    <img
+                      src={`${posts.photoURL}`}
+                      alt="img"
+                      className="object-cover w-[200px] h-full rounded-t-xl"
+                    />
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div
                   className={cn(
-                    "text-[14px] text-[#A79277] text-ellipsis line-clamp-3 text-balance"
+                    "flex items-center justify-between gap-2 p-2 w-full h-[60px] border",
+                    posts.photoURL && "h-[50px]"
                   )}
                 >
-                  {posts.content}
-                </span>
+                  <div className="flex items-center">
+                    {" "}
+                    {postUser && postUser.profileImg && (
+                      <img
+                        src={postUser.profileImg}
+                        alt="User Profile"
+                        className="w-[30px] h-[30px] rounded-full mr-2"
+                      />
+                    )}
+                    <span className="text-[#74512D] text-[14px] font-IBMSemibold">
+                      {posts.nickname}
+                    </span>
+                  </div>
+
+                  <span className="text-[12px]">좋아요 위치</span>
+                </div>
               </div>
-              <div
-                className={cn(
-                  "flex items-center justify-between gap-2 p-1 w-full h-[60px] border",
-                  posts.photoURL && "h-[50px]"
-                )}
-              >
-                <span className="text-[#74512D] text-[14px] font-IBMSemibold">
-                  {posts.nickname}
-                </span>
-                <span className="text-[12px]">좋아요 👍</span>
-              </div>
-            </div>
-          ))
+            );
+          })
         ) : (
           <p>피드가 없습니다..</p>
         )}
