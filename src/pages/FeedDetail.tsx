4e0c535205797c0df820afd17ba4bc6015e08b2b
wooohyerim <hyerim3414@gmail.com>
2024-07-20 14:00:55 +0900

    안쓰는 hook 제거

--- src/pages/FeedDetail.tsx
+++ src/pages/FeedDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate, useParams } from "react-router-dom";
 import { useForm, FormProvider } from "react-hook-form";
